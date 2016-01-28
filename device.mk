e18ff7140df0ab390f6a2cac3b379bdbfe28c2a6
DC07 <deveshnegi.3105.dn@gmail.com>
2016-01-28 07:23:12 -0500

    jalebi: Add support for 'frame-rate' key
    * From LA.BR.1.2.7_rb1.1 msm8916_32
    
    Create new media_codecs_performace.xml to add
    frame-rate key support

--- device.mk
+++ device.mk
@@ -103,6 +103,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
