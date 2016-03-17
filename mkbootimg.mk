c22f3b3f45659ff08a18e595bbc4ec7d3ae06efe
Louis Popi <theh2o64@gmail.com>
2016-03-17 21:14:47 +0100

    jalebi: Source built DT
    Signed-off-by: AudioGod <audiogod@sungsonic.co>

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -2,13 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbTool$(HOST_EXECUTABLE_SUFFIX)
-INSTALLED_DTIMAGE_TARGET := device/yu/jalebi/dt.img
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
-# Prebuilt DT Image
-#$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-#	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-#	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/dts/
-#	@echo "Made DT image: $@"
+ifneq ($(TARGET_KERNEL_ARCH),)
+KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
+else
+KERNEL_ARCH := $(TARGET_ARCH)
+endif
+
+# Build DT Image
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
+	@echo "Made DT image: $@"
 
 
 ## Overload bootimg generation: Same as the original, + --dt arg
